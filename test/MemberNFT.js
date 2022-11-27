b9ed18a3c2c4d35652431cf00c25e2844a35e3c8
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-11-27 17:55:03 +0900

    [main]: MemberNFT:NFT作成テスト実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -1,14 +1,18 @@
-const {expect} = require("Chai");
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
 
 describe("MemberNFTコントラクト", function () {
     let MemberNFT;
     let memberNFT;
     const name = "MemberNFT";
     const symbol = "MEM";
+    const tokenURI1 = "hoge1";
+    const tokenURI2 = "hoge2";
     let owner;
+    let addr1;
 
     beforeEach(async function () {
-        [owner] = await ethers.getSigners();
+        [owner, addr1] = await ethers.getSigners();
         MemberNFT = await ethers.getContractFactory("MemberNFT");
         memberNFT = await MemberNFT.deploy();
         memberNFT.deployed();
@@ -20,5 +24,18 @@ describe("MemberNFTコントラクト", function () {
     });
     it("デプロイアドレスがownerに設定されるべき", async function () {
         expect(await memberNFT.owner()).to.equal(owner.address);
+    });
+    it("ownerはNFT作成できるべき", async function () {
+        await memberNFT.nftMint(addr1.address, tokenURI1);
+        expect(await memberNFT.ownerOf(1)).to.equal(addr1.address);
+    });
+    it("NFT作成のたびにtokenIdがインクリメントされるべき", async function () {
+        await memberNFT.nftMint(addr1.address, tokenURI1);
+        await memberNFT.nftMint(addr1.address, tokenURI2);
+        expect(await memberNFT.tokenURI(1)).to.equal(tokenURI1)
+        expect(await memberNFT.tokenURI(2)).to.equal(tokenURI2)
+    });
+    it("owner以外はNFT作成に失敗すべき", async function () {
+        await expect(memberNFT.connect(addr1).nftMint(addr1.address, tokenURI1)).to.be.revertedWith("Ownable: caller is not the owner")
     });
 })
